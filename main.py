96e4e98d04745b9458aca31a68b1204ddf73881d
Bertie Brandt <hi@bertie.co.nz>
2024-10-08 12:14:08 +0100

    Basic API

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message: Hello World"}
+
+app.post("/items")
+async def create_item(item: Item):
+    return{"message": f"{item.name} is priced at ${item.price}"}
